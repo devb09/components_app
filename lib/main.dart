0414391229c417f9a6ab9578dd81e1c7c62435ea
brn091 <brn091dev@gmail.com>
2023-01-18 11:29:34 -0500

    home widget creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:components_app/screens/listview1.dart';
-import 'package:components_app/screens/listview2.dart';
+
+import 'package:components_app/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,10 +9,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: ListView2Screen(),
+      // home: const ListView2Screen(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (BuildContext context) => const HomeScreen(),
+        '/listview1': (BuildContext context) => const ListView1Screen(),
+        '/listview2': (BuildContext context) => const ListView2Screen(),
+        '/card': (BuildContext context) => const CardScreen(),
+        '/alter': (BuildContext context) => const AlertScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:components_app/screens/alert.dart';
+export 'package:components_app/screens/card.dart';
+export 'package:components_app/screens/listview1.dart';
+export 'package:components_app/screens/listview2.dart';
+export 'package:components_app/screens/home.dart';
